cb63f1ecd9a36af41f11bfbdcd81e069a8ad7518
Omar SallaM <omarsallam906@gmail.com>
2026-09-20 12:45:36 +0300

    Time: 1 ms (98.56%), Space: 42.4 MB (21.87%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+
+        for(int i = 0; i < s.Length; ++i) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                stack.Push(s[i]);
+            else {
+                if(stack.Count == 0) return false;
+
+                char lastOpen = stack.Pop();
+                if((lastOpen == '(' && s[i] != ')') ||
+                   (lastOpen == '{' && s[i] != '}') ||
+                   (lastOpen == '[' && s[i] != ']'))
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
